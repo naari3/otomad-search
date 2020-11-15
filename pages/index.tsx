e55cf097ddac71f6304fcc7c23a3b0967c73ce3f
naari3 <naari.named@gmail.com>
2020-11-16 00:26:05 +0900

    Initialized

---
+++ pages/index.tsx
@@ -0,0 +1,31 @@
+import React, { FC } from "react"; // {FC} をimport対象に追加
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+const Home: FC = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <header className={styles.header}>
+        <h1>otomad-search</h1>
+      </header>
+
+      <main className={styles.main}>
+        <div>
+          <label>
+            マイリスト数下限: <input type="number"></input>
+          </label>
+          <button>検索</button>
+        </div>
+      </main>
+
+      <footer className={styles.footer}></footer>
+    </div>
+  );
+};
+
+export default Home;
