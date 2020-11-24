4a4d34ea66f096b283501881a56e90d13ce2aa22
naari3 <naari.named@gmail.com>
2020-11-24 14:14:11 +0900

    Implement tags on sp

---
+++ components/TagList.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useRef } from "react";
+import styles from "./TagList.module.css";
+import Tag from "./TagOnScreen";
+
+type Props = {
+  tags: string[];
+};
+
+const TagList = React.memo(({ tags }: Props) => {
+  const ref = useRef<HTMLUListElement>(null);
+
+  const filteredTags = (tags: string[]): string[] =>
+    tags.filter(
+      (tag) => !["音MAD", "音mad", "音ＭＡＤ", "音ｍａｄ"].includes(tag)
+    );
+
+  useEffect(() => {
+    if (ref.current) {
+      const event = new Event("scroll");
+      ref.current.addEventListener("scroll", () => window.dispatchEvent(event));
+    }
+  }, []);
+
+  return (
+    <ul className={styles.tags} ref={ref}>
+      {filteredTags(tags).map((tag) => (
+        <li className={styles.tag} key={tag}>
+          <Tag name={tag} />
+        </li>
+      ))}
+    </ul>
+  );
+});
+
+export default TagList;
