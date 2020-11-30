d55a7ca025f8c1ee6f9e1ddd176f8918362bfeef
naari3 <naari.named@gmail.com>
2020-11-30 12:39:52 +0900

    DON'T MERGE

--- lib/date.ts
+++ lib/date.ts
@@ -3,5 +3,5 @@ import { ja } from "date-fns/locale";
 
 export default function formattedDate(dateString: string): string {
   const d = parseISO(dateString);
-  return format(d, "yyyy/MM/dd hh:mm", { locale: ja });
+  return format(d, "yyyy/MM/dd hh:mm", { locale: ja});
 }
