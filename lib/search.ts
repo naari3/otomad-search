927e42a1f06b716e0f1c56cae3ff3685ff593806
naari3 <naari.named@gmail.com>
2024-04-02 18:20:53 +0900

    Follow up changing API URL

--- lib/search.ts
+++ lib/search.ts
@@ -1,6 +1,6 @@
 /* eslint @typescript-eslint/no-unused-vars: 0 */
 // https://site.nicovideo.jp/search-api-docs/search.html
-const BASE_URL = "https://api.search.nicovideo.jp/";
+const BASE_URL = "https://snapshot.search.nicovideo.jp/";
 
 import * as Sentry from "@sentry/node";
 
